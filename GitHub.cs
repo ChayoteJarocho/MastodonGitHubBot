1b9c5bbae5afd4d7b9044aeef9e95327ac2692ef
ChayoteJarocho <77412126+ChayoteJarocho@users.noreply.github.com>
2022-11-20 02:09:11 -0800

    Fully functional code that retrieves the access tokens if they don't exist, and creates the mastodon and github client instances successfully.

---
+++ GitHub.cs
@@ -0,0 +1,52 @@
+﻿using Octokit;
+using System;
+using System.Threading.Tasks;
+
+namespace MastodonGitHubBot
+{
+    internal static class GitHub
+    {
+        public static async Task<GitHubClient> GetClientAsync(Settings settings)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(settings.GitHubClientId);
+            ArgumentException.ThrowIfNullOrEmpty(settings.GitHubSecret);
+            ArgumentException.ThrowIfNullOrEmpty(settings.GitHubUserName);
+
+            var github = new GitHubClient(new ProductHeaderValue(settings.AppName));
+            string accessToken = await GetAccessTokenAsync(settings, github);
+            github.Credentials = new Credentials(accessToken);
+
+            return github;
+        }
+
+        private static async Task<string> GetAccessTokenAsync(Settings settings, GitHubClient github)
+        {
+            if (string.IsNullOrWhiteSpace(settings.GitHubAccessToken))
+            {
+                OauthLoginRequest oauthLoginRequest = new(settings.GitHubClientId);
+                oauthLoginRequest.Scopes.Add(settings.GitHubUserName);
+                oauthLoginRequest.Scopes.Add("public_repo");
+
+                string authCode = GetGitHubOAuthCode(github, oauthLoginRequest);
+
+                OauthTokenRequest oauthTokenRequest = new(settings.GitHubClientId, settings.GitHubSecret, authCode);
+                OauthToken accessToken = await github.Oauth.CreateAccessToken(oauthTokenRequest);
+
+                settings.GitHubAccessToken = accessToken.AccessToken;
+            }
+
+            return settings.GitHubAccessToken;
+        }
+
+        private static string GetGitHubOAuthCode(GitHubClient github, OauthLoginRequest oauthLoginRequest)
+        {
+            Uri url = github.Oauth.GetGitHubLoginUrl(oauthLoginRequest);
+            Console.WriteLine($"Go to the authorization page: {url}");
+            Console.Write("Paste the authentication code: ");
+            string authCode = Console.ReadLine() ?? throw new NullReferenceException(nameof(authCode));
+            ArgumentException.ThrowIfNullOrEmpty(authCode);
+
+            return authCode;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using Mastonet;
+using Octokit;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MastodonGitHubBot;
+
+public class Program
+{
+    private static readonly string SettingsFilePath = "appsettings.json";
+    private static async Task Main()
+    {
+        Settings settings = LoadSettings();
+
+        HttpClient sharedHttpClient = new HttpClient();
+
+        MastodonClient mastodon = await Mastodon.GetClientAsync(settings, sharedHttpClient);
+
+        GitHubClient github = await GitHub.GetClientAsync(settings);
+
+        FlushSettings(settings);
+
+       // Status status = await mastodon.PublishStatus("Test", Visibility.Private);
+    }
+
+    private static Settings LoadSettings()
+    {
+        if (!File.Exists(SettingsFilePath))
+        {
+            throw new FileNotFoundException(SettingsFilePath);
+        }
+
+        string json = File.ReadAllText(SettingsFilePath);
+        Settings settings = JsonSerializer.Deserialize<Settings>(json) ?? throw new JsonException($"Malformed {SettingsFilePath}.");
+
+        string? message = null;
+
+        if (string.IsNullOrWhiteSpace(settings.AppName))
+        {
+            message = $"AppName is not set in {SettingsFilePath}. It needs to be a fixed value that the OAuth services can recognize.";
+        }
+        if (string.IsNullOrWhiteSpace(settings.GitHubClientId))
+        {
+            message = $"GitHubClientId is not set in {SettingsFilePath}. You must create an OAuth application on GitHub, copy the ClientId value, then paste it in {SettingsFilePath}. The apps are created here: https://github.com/settings/applications/new";
+        }
+        if (string.IsNullOrWhiteSpace(settings.GitHubSecret))
+        {
+            message = $"GitHubSecret is not set in {SettingsFilePath}. You must create an OAuth application on GitHub, generate a secret for that app, then paste the value in {SettingsFilePath}. The apps are created here: https://github.com/settings/applications/new";
+        }
+        if (string.IsNullOrWhiteSpace(settings.GitHubUserName))
+        {
+            message = $"GitHubUserName is not set in {SettingsFilePath}. It needs to be set to the user account handle that will be executing the requests.";
+        }
+        if (string.IsNullOrWhiteSpace(settings.MastodonServer))
+        {
+            message = $"MastodonServer is not set in {SettingsFilePath}. It needs to be set to the name of the Mastodon server that hosts the automated account.";
+        }
+
+        if (message != null)
+        {
+            throw new InvalidDataException(message);
+        }
+
+        return settings;
+    }
+
+    private static void FlushSettings(Settings settings)
+    {
+        string json = JsonSerializer.Serialize(settings);
+        File.WriteAllText(SettingsFilePath, json);
+    }
+}
