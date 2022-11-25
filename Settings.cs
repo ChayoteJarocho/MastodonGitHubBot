877884373ad557e7bf71f990cb945176912e8cb2
ChayoteJarocho <77412126+ChayoteJarocho@users.noreply.github.com>
2022-11-24 22:24:05 -0800

    Add prompts for all the empty json values.

--- Settings.cs
+++ Settings.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text.Json;
 
 namespace MastodonGitHubBot
@@ -12,6 +13,8 @@ namespace MastodonGitHubBot
         public string GitHubSecret { get; set; } = string.Empty;
         public string GitHubAccessToken { get; set; } = string.Empty;
         public string GitHubUserName { get; set; } = string.Empty;
+        public string GitHubRepoOrg { get; set; } = string.Empty;
+        public string GitHubRepoName { get; set; } = string.Empty;
         public string MastodonServer { get; set; } = string.Empty;
         public string MastodonAccessToken { get; set; } = string.Empty;
 
@@ -32,35 +35,112 @@ namespace MastodonGitHubBot
             string json = File.ReadAllText(SettingsFilePath);
             Settings settings = JsonSerializer.Deserialize<Settings>(json) ?? throw new JsonException($"Malformed {SettingsFilePath}.");
 
-            string? message = null;
-
             if (string.IsNullOrWhiteSpace(settings.AppName))
             {
-                message = $"AppName is not set in {SettingsFilePath}. It needs to be a fixed value that the OAuth services can recognize.";
+                settings.AppName = AskString("name of the GitHub application reserved for the OAuth services. If you haven't created it, create it in https://github.com/settings/applications/new. After creating it, make sure to also store the client and the secret values");
             }
             if (string.IsNullOrWhiteSpace(settings.GitHubClientId))
             {
-                message = $"GitHubClientId is not set in {SettingsFilePath}. You must create an OAuth application on GitHub, copy the ClientId value, then paste it in {SettingsFilePath}. The apps are created here: https://github.com/settings/applications/new";
+                settings.GitHubClientId = AskString("value of the GitHub OAuth application's ClientId. You should have gotten one after creating your application");
             }
             if (string.IsNullOrWhiteSpace(settings.GitHubSecret))
             {
-                message = $"GitHubSecret is not set in {SettingsFilePath}. You must create an OAuth application on GitHub, generate a secret for that app, then paste the value in {SettingsFilePath}. The apps are created here: https://github.com/settings/applications/new";
+                settings.GitHubSecret = AskString("value of the GitHub OAuth application's secret. You should have gotten one after creating your application");
             }
             if (string.IsNullOrWhiteSpace(settings.GitHubUserName))
             {
-                message = $"GitHubUserName is not set in {SettingsFilePath}. It needs to be set to the user account handle that will be executing the requests.";
+                settings.GitHubUserName = AskString("value of the GitHub account handle that will be used to execute requests");
+            }
+            if (string.IsNullOrWhiteSpace(settings.GitHubRepoOrg))
+            {
+                settings.GitHubRepoOrg = AskString("value of the GitHub org or user that owns the target repo");
+            }
+            if (string.IsNullOrWhiteSpace(settings.GitHubRepoName))
+            {
+                settings.GitHubRepoName = AskString("value of the target GitHub repo name");
             }
             if (string.IsNullOrWhiteSpace(settings.MastodonServer))
             {
-                message = $"MastodonServer is not set in {SettingsFilePath}. It needs to be set to the name of the Mastodon server that hosts the automated account.";
+                settings.MastodonServer = AskString("name of the Mastodon server that hosts your automated account");
             }
-
-            if (message != null)
+            if (string.IsNullOrWhiteSpace(settings.GitHubAccessToken))
             {
-                throw new InvalidDataException(message);
+                settings.GitHubAccessToken = AskOptionalString("GitHub access token");
+            }
+            if (string.IsNullOrWhiteSpace(settings.MastodonAccessToken))
+            {
+                settings.GitHubAccessToken = AskOptionalString("Mastodon access token");
             }
 
             return settings;
+        }
+
+        private static string AskString(string description)
+        {
+            string? value = null;
+
+            while (string.IsNullOrWhiteSpace(value))
+            {
+                Console.Write($"Please provide the {description}: ");
+                value = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("Incorrect value (null, empty or whitespace). Try again.");
+                }
+                else
+                {
+                    string? answer = null;
+                    while (string.IsNullOrWhiteSpace(answer))
+                    {
+                        Console.Write($"You typed [{value}]. Is this correct? [Y/N]: ");
+                        answer = Console.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(answer))
+                        {
+                            switch (answer)
+                            {
+                                case "n" or "N":
+                                    Console.WriteLine("Ok, starting over.");
+                                    value = null;
+                                    break;
+                                case "y" or "Y":
+                                    Console.WriteLine($"Will save [{value}] into the json file.");
+                                    break;
+                                default:
+                                    Console.WriteLine("Unexpected value. Asking again.");
+                                    answer = null;
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return value;
+        }
+
+        private static string AskOptionalString(string description)
+        {
+            string value = string.Empty;
+            string? answer = null;
+            while (string.IsNullOrWhiteSpace(answer))
+            {
+                Console.Write($"Do you already have a {description}? [Y/N]: ");
+                answer = Console.ReadLine();
+                switch (answer)
+                {
+                    case "n" or "N":
+                        Console.WriteLine("No problem. Will go through the authentication process.");
+                        break;
+                    case "y" or "Y":
+                        value = AskString(description);
+                        break;
+                    default:
+                        Console.WriteLine("Unexpected value. Asking again.");
+                        answer = null;
+                        break;
+                }
+            }
+            return value;
         }
     }
 }
